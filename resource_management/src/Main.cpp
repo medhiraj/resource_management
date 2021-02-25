dc2996d28f23a883821367e588c545d53331db2c
Dhiraj Bhandare <dhiraj.bhandare@gmail.com>
2021-02-25 18:35:50 +0100

    Creatring  template for program

---
+++ resource_management/src/Main.cpp
@@ -0,0 +1,28 @@
+
+
+
+/**
+ * @file main.cppcc
+ *
+ * @This is the entry point for program ,It's test for resorce mangemner
+ *
+ *
+ * @author Dhiraj Bhandare
+ * Contact: dhiraj.bhandare@gmail.com
+ *
+ */
+#include <iostream> 
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	//cout << "You have entered " << argc
+	//	<< " arguments:" << "\n";
+
+	//for (int i = 0; i < argc; ++i)
+	//	cout << argv[i] << "\n";
+
+	cout << "Resource Management" << endl;
+
+	return 0;
+}
